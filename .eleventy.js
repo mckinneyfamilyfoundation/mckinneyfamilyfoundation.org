6edb24f07ccb01b7c6c44e6e8e874ec0e51f9cb9
avblackwell <alexvblackwell@gmail.com>
2022-08-13 14:43:58 -0400

    progress on landing page

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/images/");
+
   eleventyConfig.addWatchTarget("./src/css/");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
