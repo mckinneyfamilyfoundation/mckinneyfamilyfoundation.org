3344f2bd883da6dd73b494b3a4628e0d5b2d1c4a
avblackwell <alexvblackwell@gmail.com>
2022-09-06 17:08:14 -0400

    fix styles not being built TODO images

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/assets/");
-  eleventyConfig.addPassthroughCopy("./src/images/");
+  eleventyConfig.addPassthroughCopy("./src/assets/css");
+  eleventyConfig.addPassthroughCopy("./src/assets/images");
+  eleventyConfig.addPassthroughCopy("./src/assets/js");
+
+  
+
 
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
