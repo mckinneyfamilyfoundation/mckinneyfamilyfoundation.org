e54c53ec5a795421cf3fdbb9d33c9241ef57b2ec
avblackwell <alexvblackwell@gmail.com>
2022-09-06 18:47:31 -0400

    add lazy loading to images

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/assets');
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addPassthroughCopy('src/images');
 
 
